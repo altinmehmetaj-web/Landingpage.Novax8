91f10d0fcf440ab610ce9a44c534417cc00fb378
shopping influencer <altinmehmetaj@gmail.com>
2026-09-18 09:31:52 +0200

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,9 @@
+/* CONFIGURAZIONE
+   Sostituisci i valori qui sotto prima della pubblicazione.
+*/
+const LANDING_CONFIG = {
+  applicationUrl: "",   // Se hai un modulo esterno (es. Calendly/Typeform), inseriscilo qui.
+  email: "INSERISCI_EMAIL",
+  whatsapp: "INSERISCI_NUMERO",
+  vslPath: "assets/videos/vsl.mp4"
+};
